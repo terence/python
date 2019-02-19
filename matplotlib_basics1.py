94e726d2783a87c14748d82acce3b950ded63505
Terence <terence@192-168-1-4.tpgi.com.au>
2019-02-19 17:32:41 +1100

    more plots

--- matplotlib_basics1.py
+++ matplotlib_basics1.py
@@ -143,5 +143,40 @@ plt.subplot(2,2,1)
 plt.subplot(2,2,3)
 plt.subplot(2,2,4)
 
-plt.show()
+#plt.show()
 plt.savefig("output/mpl_multi1.png", transparent=True)
+
+
+# Bar Charts
+# ==========================
+n = 12
+X = np.arange(n)
+Y1 = (1-X/float(n)) * np.random.uniform(0.5,1.0,n)
+Y2 = (1-X/float(n)) * np.random.uniform(0.5,1.0,n)
+
+plt.bar(X, +Y1, facecolor='#9999ff', edgecolor='white')
+plt.bar(X, -Y2, facecolor='#ff9999', edgecolor='white')
+
+for x,y in zip(X,Y1):
+    plt.text(x+0.4, y+0.05, '%.2f' % y, ha='center', va= 'bottom')
+
+plt.ylim(-1.25,+1.25)
+#plt.show()
+plt.savefig("output/mpl_barcharts1.png", transparent=True)
+
+# Contours
+# ==================
+
+def f(x,y): return (1-x/2+x**5+y**3)*np.exp(-x**2-y**2)
+
+n = 256
+x = np.linspace(-3,3,n)
+y = np.linspace(-3,3,n)
+X,Y = np.meshgrid(x,y)
+
+plt.contourf(X, Y, f(X,Y), 8, alpha=.75, cmap='jet')
+C = plt.contour(X, Y, f(X,Y), 8, colors='black', linewidth=.5)
+# plt.show()
+plt.savefig("output/mpl_contours1.png", transparent=True)
+
+
